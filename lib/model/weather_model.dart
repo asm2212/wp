9b68f1603e222e28c85d1171407b532b63c0e1df
asm2212 <asmareadmasu0@gmail.com>
2024-05-31 15:46:48 +0300

    create model

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,29 @@
+class Weather{
+  String? cityName;
+  double? temp;
+  double? wind;
+  int? humidity;
+  double? feels_like;
+  int? pressure;
+
+  Weather(
+    {
+      this.cityName,
+      this.temp,
+      this.wind,
+      this.humidity,
+      this.feels_like,
+      this.pressure,
+    }
+  );
+  factory Weather.fromJson(Map<String, dynamic> json){
+    return Weather(
+      cityName: json['name'],
+      temp: json['main']['temp'],
+      wind: json['wind']['speed'],
+      humidity: json['main']['humidity'],
+      feels_like: json['main']['feels_like'],
+      pressure: json['main']['pressure'],
+    );
+  }
+}
